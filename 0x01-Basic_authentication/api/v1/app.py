c16b827af115d0e72e7405b02187808c4b71bf67
John <johnnjau202@gmail.com>
2024-07-11 19:49:37 +0300

    index.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,8 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.views import app_views
-from api.v1.auth.auth import Auth  # Ensure this import is correct
 
 
 app = Flask(__name__)
