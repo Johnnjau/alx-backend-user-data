7a29853ee041d02cdf7456b600763f6b6155356c
John <johnnjau202@gmail.com>
2024-07-13 16:23:14 +0300

    session_auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Module of SessionAuth class"""
+from api.v1.auth.auth import Auth
+import uuid
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """SessionAuth class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create_session method"""
+        if not user_id:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """user_id_for_session_id method"""
+        if not session_id:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
+
+    def current_user(self, request=None):
+        """current_user method"""
+        cookies = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookies)
+        user = User()
+        return user.get(user_id)
